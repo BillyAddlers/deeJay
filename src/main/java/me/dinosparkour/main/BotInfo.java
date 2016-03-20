65ab1ac1909be65e754bef0aecfa73d211e54822
Dinos <emeraldpmc@gmail.com>
2016-03-20 23:17:32 +0200

    #JustIndentationThings

--- src/main/java/me/dinosparkour/main/BotInfo.java
+++ src/main/java/me/dinosparkour/main/BotInfo.java
@@ -5,24 +5,24 @@ import org.json.JSONObject;
 
 public class BotInfo {
 
-	private static JSONObject jsonObject = ConfigHandler.getConfig();
-	private static String email = jsonObject.getString("email");
-	private static String password = jsonObject.getString("password");
-	private static String prefix = jsonObject.getString("prefix");
+    private static JSONObject jsonObject = ConfigHandler.getConfig();
+    private static String email = jsonObject.getString("email");
+    private static String password = jsonObject.getString("password");
+    private static String prefix = jsonObject.getString("prefix");
 
-	public static String getAuthorId() {
-		return "98457903660269568";
-	}
+    public static String getAuthorId() {
+        return "98457903660269568";
+    }
 
-	static String getEmail() {
-		return BotInfo.email;
-	}
+    static String getEmail() {
+        return BotInfo.email;
+    }
 
-	static String getPassword() {
-		return BotInfo.password;
-	}
+    static String getPassword() {
+        return BotInfo.password;
+    }
 
-	public static String getPrefix() {
-		return BotInfo.prefix;
-	}
+    public static String getPrefix() {
+        return BotInfo.prefix;
+    }
 }
--- src/main/java/me/dinosparkour/utilities/ConfigHandler.java
+++ src/main/java/me/dinosparkour/utilities/ConfigHandler.java
@@ -9,52 +9,52 @@ import java.nio.file.Paths;
 
 public class ConfigHandler {
 
-	private static File config = new File("config.json");
+    private static File config = new File("config.json");
 
-	public static JSONObject getConfig() {
-		if(!config.exists()) {
-			try {
-				ConfigHandler.create();
+    public static JSONObject getConfig() {
+        if(!config.exists()) {
+            try {
+                ConfigHandler.create();
 
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
-			System.out.println("Created a configuration file. Please fill the login credentials!");
-			System.exit(0);
-		}
+            System.out.println("Created a configuration file. Please fill the login credentials!");
+            System.exit(0);
+        }
 
-		JSONObject object = null;
-		try {
-			object = ConfigHandler.load();
+        JSONObject object = null;
+        try {
+            object = ConfigHandler.load();
 
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-		return object;
-	}
+        return object;
+    }
 
-	private static void create() throws IOException {
-		Files.write(Paths.get(config.getPath()),
-				new JSONObject()
-						.put("email", "")
-						.put("password", "")
-						.put("prefix", "!")
-						.toString(4).getBytes());
-	}
+    private static void create() throws IOException {
+        Files.write(Paths.get(config.getPath()),
+                new JSONObject()
+                        .put("email", "")
+                        .put("password", "")
+                        .put("prefix", "!")
+                        .toString(4).getBytes());
+    }
 
-	private static JSONObject load() throws IOException {
-		JSONObject object = new JSONObject(new String(Files.readAllBytes(Paths.get(config.getPath())), "UTF-8"));
+    private static JSONObject load() throws IOException {
+        JSONObject object = new JSONObject(new String(Files.readAllBytes(Paths.get(config.getPath())), "UTF-8"));
 
-		if(object.has("email") && object.has("password") && object.has("prefix"))
-			return object;
+        if(object.has("email") && object.has("password") && object.has("prefix"))
+            return object;
 
-		ConfigHandler.create();
-		System.err.println("The config file was missing a value! [Either email/password/prefix]");
-		System.out.println("Regenerating the file from scratch..");
-		System.exit(1);
+        ConfigHandler.create();
+        System.err.println("The config file was missing a value! [Either email/password/prefix]");
+        System.out.println("Regenerating the file from scratch..");
+        System.exit(1);
 
-		return null;
-	}
+        return null;
+    }
 }
