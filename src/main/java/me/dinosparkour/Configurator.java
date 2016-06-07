7518690ff74b7802b127f6fe78871d72f22eac9a
Dinos <emeraldpmc@gmail.com>
2016-06-07 14:37:27 +0300

    Fuck JavaDocs

--- src/main/java/me/dinosparkour/Configurator.java
+++ src/main/java/me/dinosparkour/Configurator.java
@@ -11,11 +11,6 @@ class Configurator {
 
     private static final File config = new File("config.json");
 
-    /**
-     * Get the contents of the config file if it exists, or create a new one if it doesn't
-     *
-     * @return JSONObject config
-     */
     static JSONObject getConfig() {
         if(!config.exists()) {
             try {
@@ -40,11 +35,6 @@ class Configurator {
         return object;
     }
 
-    /**
-     * Create a new config file
-     *
-     * @throws IOException
-     */
     private static void create() throws IOException {
         Files.write(Paths.get(config.getPath()),
                 new JSONObject()
@@ -54,12 +44,6 @@ class Configurator {
                         .toString(4).getBytes());
     }
 
-    /**
-     * Load the contents of the config file, or regenerate it if it's missing a value
-     * Used internally by the configurator
-     * @return JSONObject config
-     * @throws IOException
-     */
     private static JSONObject load() throws IOException {
         JSONObject object = new JSONObject(new String(Files.readAllBytes(Paths.get(config.getPath())), "UTF-8"));
 
